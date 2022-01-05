06c0db60690ee15acc75d92981ff69c33edef5af
Rishab42 <rishab0197@gmail.com>
2022-01-06 01:09:51 +0530

    Commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import{useState,useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [id,setId]=useState("")
+  const [output,setOutput]=useState('');
+
+  const getdata=()=>{
+      axios.get('https://the-dune-api.herokuapp.com/quotes').then(res=>{
+        console.log(res);
+        setId(res.data[0].id);
+        setOutput(res.data[0].quote);
+      })
+  }
+
+return(
+  <div className="container">
+    <div className="box">
+      <div className="id">ID:-  {id}</div>
+      <div className="text">QUOTE:-  {output}</div>
     </div>
-  );
+    <button onClick={(e)=>getdata()}>Get Quote</button>
+  </div>
+)
+
 }
 
 export default App;
